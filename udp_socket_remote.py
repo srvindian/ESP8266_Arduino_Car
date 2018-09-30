76c18437b7f2b428ab28dca1618947e33b94931d
Sourav Mondal <35125992+srvindian@users.noreply.github.com>
2018-09-30 20:05:50 +0530

    Update udp_socket_remote.py

--- udp_socket_remote.py
+++ udp_socket_remote.py
@@ -1,34 +1,32 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Sep 25 21:11:42 2018
+Created on Sun Sep 30 16:19:30 2018
 
 @author: Sourav
 """
-
+import cv2
+import numpy as np
 import socket
+#%%
 UDP_IP = '192.168.4.1'
 UDP_PORT = 80
 BUFFER_SIZE = 1024
-#MESSAGE = b'%0'
-
-if __name__ == "__main__":
-    s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#    s.connect((UDP_IP, UDP_PORT))
-    # 0-stop,1-Left,2-Right,3-Forward,4-Backward
-    while True:
-        x = input('(0-4) : ')
-        if x=='x':
-            break
-        MESSAGE = '%'+x+'\r\n'
+s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+move={-1:0,97:1,100:2,119:3,115:4,27:5}
+orient={0:'Stop',1:'Left',2:'Right',3:'Forward',4:'Backward'}
+while True:
+    img=np.zeros((100,200,3),dtype=np.uint8)
+    key=cv2.waitKey(200)
+    #0-stop,1-Left,2-Right,3-Forward,4-Backward
+    if key!=27:
+        x=move[key]
+        print(orient[x])
+        MESSAGE = '%'+str(x)+'\r\n'
+#        print(MESSAGE)
         s.sendto(MESSAGE.encode(),(UDP_IP, UDP_PORT))
-#    data = s.recv(BUFFER_SIZE)
-    # -*- coding: utf-8 -*-
-
-
-
-#For arduino<esp8266>
-#  sendData("AT+RST\r\n",2000,DEBUG); // reset module
-#  sendData("AT+CWMODE=3\r\n",1000,DEBUG); // configure as access point
-#  sendData("AT+CIPMUX=1\r\n",1000,DEBUG); // configure for multiple connections
-#  sendData("AT+CIPSTART=0,\"UDP\",\"192.168.4.1\",80,80,2\r\n",1000,DEBUG);
-#  sendData("AT+CIPSEND=0,7,\"192.168.4.1\",80",1000,DEBUG); // turn on server on port 80
+        cv2.putText(img,orient[x], (50,50), cv2.FONT_HERSHEY_SIMPLEX,1, (0,255,0), 4)
+        cv2.imshow("Remote",img)
+    else:
+        break
+cv2.destroyAllWindows()
+    
